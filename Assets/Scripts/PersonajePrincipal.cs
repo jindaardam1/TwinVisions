31924077428f7b1a7fb514e9a04cb7803632bc07
jindaardam1 <jindaardam1@gmail.com>
2023-11-02 10:45:45 +0100

    Primeras pruebas

--- Assets/Scripts/PersonajePrincipal.cs
+++ Assets/Scripts/PersonajePrincipal.cs
@@ -1,21 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PersonajePrincipal : MonoBehaviour
 {
     public Rigidbody2D rb2d;
+    public float velocidad = 5.0f;
+    public float fuerzaSalto = 10.0f;
+    public bool enSuelo;
 
-    public double velocidad = 5;
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        rb2d.GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        enSuelo = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        var movimientoHorizontal = Input.GetAxis("Horizontal");
+
+        var movimiento = new Vector2(movimientoHorizontal, -rb2d.gravityScale);
+
+        rb2d.velocity = movimiento * velocidad;
         
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            enSuelo = true; // El personaje está en el suelo
+        }
     }
 }
